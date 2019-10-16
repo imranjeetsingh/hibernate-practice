572617cb2ecab1c919f9a5db57820e591cc6f664
imranjeetsingh <ranjee970@gmail.com>
2019-10-16 17:13:44 +0530

    hibernate-query-list

---
+++ src/com/ranjeet/hibernate/demo/ReadStudentDemo.java
@@ -0,0 +1,52 @@
+package com.ranjeet.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ranjeet.hibernate.entity.Student;
+
+public class ReadStudentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			System.out.println("Reading stident object ...");
+			
+			session.beginTransaction();
+			
+			List<Student> students = session.createQuery("from Student").getResultList();
+			
+			DisplayStudent(students);
+			
+			students = session.createQuery("from Student s where s.id='1'")
+					 .getResultList();
+			
+			DisplayStudent(students);
+			
+			session.getTransaction().commit();
+			
+			
+			System.out.println("Done!!!!!!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+	private static void DisplayStudent(List<Student> students) {
+		for(Student data:students) {
+			System.out.println(data);
+		}
+	}
+
+}
